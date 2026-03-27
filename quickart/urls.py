d31423539a1feb9210fe1cd296b22f18398f035a
jatinsuryansh <jatinsuryansh4304@gmail.com>
2026-03-27 12:55:31 +0530

    final csrf stable fix

--- quickart/urls.py
+++ quickart/urls.py
@@ -17,11 +17,22 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Home Page Working ✅")
+
+urlpatterns = [
+    path('', home),
+    path('admin/', admin.site.urls),
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls'))
 ] 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    
+    
     
